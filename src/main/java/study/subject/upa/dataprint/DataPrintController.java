68a41f1df1c73422e0ba23125f773165490c0aaf
D.W Kang <kdy0185@gmail.com>
2022-03-10 19:45:13 +0900

    Chore #2 - 클래스 및 패키지 변경
    * util 관련 기능 분리

--- src/main/java/study/subject/upa/dataprint/DataPrintController.java
+++ src/main/java/study/subject/upa/dataprint/DataPrintController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import study.subject.upa.util.DataUtil;
 
 /**
  * @Class : DataPrintController
@@ -31,7 +32,7 @@ public class DataPrintController {
 
         // 노출 유형 - H : Html 태그 제외, T : Text 전체
         if ("H".equals(dataType)) {
-            resultData = dataPrintService.deleteHtml(resultData);
+            resultData = DataUtil.deleteHtml(resultData);
         }
 
         return resultData;
---
+++ src/main/java/study/subject/upa/util/DataUtil.java
@@ -0,0 +1,59 @@
+package study.subject.upa.util;
+
+/**
+ * @Class : DataUtil
+ * @Author : KDW
+ * @Date : 2022-03-10
+ * @Description : Data 관련 util 클래스
+ */
+public class DataUtil {
+
+    /**
+     * <p>HTML 태그 제거</p>
+     *
+     * @param object (변경할 문자형 객체)
+     * @return String (변경 후 문자)
+     */
+    public static String deleteHtml(Object object) {
+        if (isBlank(object)) {
+            return "";
+        }
+        String str = String.valueOf(object);
+        str = str.replaceAll("<(/)?([a-zA-Z]*)(\\s[a-zA-Z]*=[^>]*)?(\\s)*(/)?>", "");
+//        str = str.replaceAll("\r\n", "");
+//        str = str.replaceAll("\r", "");
+//        str = str.replaceAll("\n", "");
+//        str = str.replaceAll("\t", "");
+        return str;
+    }
+
+    /**
+     * <p>null 및 공백 체크</p>
+     *
+     * <pre>
+     * DataUtil.isBlank(null)    = true
+     * DataUtil.isBlank("")      = true
+     * DataUtil.isBlank("  ")    = true
+     * DataUtil.isBlank(" bob ") = false
+     * DataUtil.isBlank(123)     = false
+     * </pre>
+     *
+     * @param object (체크할 문자형, 숫자형 객체)
+     * @return boolean (체크 결과)
+     */
+    public static boolean isBlank(Object object) {
+        if (object == null) {
+            return true;
+        }
+        String str = String.valueOf(object);
+        if (str.length() == 0) {
+            return true;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if ((!Character.isWhitespace(str.charAt(i)))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
