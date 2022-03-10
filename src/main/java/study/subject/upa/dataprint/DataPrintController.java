a9d1fa6fb67087d2b67f8b7e7f8ad011d57794e0
D.W Kang <kdy0185@gmail.com>
2022-03-10 17:46:28 +0900

    Feat #1 - 기본 URL 파싱 기능 구현

--- src/main/java/study/subject/upa/dataprint/DataPrintController.java
+++ src/main/java/study/subject/upa/dataprint/DataPrintController.java
@@ -1,5 +1,34 @@
 package study.subject.upa.dataprint;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DataPrintController {
 
+    @GetMapping("/api/v1/basicParsing")
+    public String basicParsing(@RequestParam("url") String paramUrl) {
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            URL url = new URL(paramUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return sb.toString();
+    }
 }
