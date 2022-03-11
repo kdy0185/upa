41a82cd704e4e1efc6c54ea44c5150d971d624d8
D.W Kang <kdy0185@gmail.com>
2022-03-11 19:56:26 +0900

    Style #8 - 테스트 코드 이동
    * CustomStreamSupportTest.interleaveTest()

--- src/test/java/study/subject/upa/util/DataUtilTest.java
+++ src/test/java/study/subject/upa/util/DataUtilTest.java
@@ -32,18 +32,4 @@ class DataUtilTest {
         assertThat(flag4).isFalse();
         assertThat(flag5).isFalse();
     }
-
-    @Test
-    public void interleaveTest() throws Exception {
-        // given
-        Stream<String> a = Stream.of("a", "c", "e");
-        Stream<String> b = Stream.of("b", "d", "f");
-
-        // when
-        Stream<String> ab = CustomStreamSupport.interleave(a, b);
-
-        // then
-        String result = ab.collect(Collectors.joining());
-        assertThat(result).isEqualTo("abcdef");
-    }
 }
---
+++ src/test/java/study/subject/upa/util/support/CustomStreamSupportTest.java
@@ -0,0 +1,24 @@
+package study.subject.upa.util.support;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+
+class CustomStreamSupportTest {
+
+    @Test
+    public void interleaveTest() throws Exception {
+        // given
+        Stream<String> a = Stream.of("a", "c", "e");
+        Stream<String> b = Stream.of("b", "d", "f");
+
+        // when
+        Stream<String> ab = CustomStreamSupport.interleave(a, b);
+
+        // then
+        String result = ab.collect(Collectors.joining());
+        assertThat(result).isEqualTo("abcdef");
+    }
+}
